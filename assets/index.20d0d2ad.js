779c65c6ae4994c9566fbd6e6d99e7fe1662d9cc
JS-banana <JS-banana@users.noreply.github.com>
2021-12-11 04:26:58 +0000

    deploy: 74b1983c7a946b2fb1a95afcd3870a13db96fa9b

---
+++ assets/index.20d0d2ad.js
@@ -0,0 +1,1 @@
+import{d as s,N as n,u,aB as p}from"./vendor.c4996dba.js";var d=s({setup(){const e=n(),o=u();return p(()=>{const{params:t,query:r}=e,{path:a}=t;o.replace({path:"/"+a,query:r})}),()=>""}});export{d as default};
