9b8c8876d61ebbdda7b16cc10aa19083517eceb2
sunss <sss@dataqin.com>
2022-03-09 16:34:48 +0800

    fix(router): 域名二级目录的路由配置优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,9 @@ import routes from './router.config';
 
 // app router
 export const router = createRouter({
-  history: createWebHashHistory(),
+  // 解决 二级路径存在时，路径地址路由不匹配的问题
+  // https://juejin.cn/post/7051826951463370760#heading-27
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
   strict: true,
   scrollBehavior: () => ({ left: 0, top: 0 }),
