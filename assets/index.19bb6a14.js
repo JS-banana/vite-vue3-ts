ab95bd3e3c0653b631ce40300a733a0849d3d809
JS-banana <JS-banana@users.noreply.github.com>
2022-06-07 08:33:05 +0000

    deploy: b209187b5df2efa41c352b68ea51d66465476bd9

--- assets/index.19bb6a14.js
+++
@@ -1,1 +0,0 @@
-import{j as s,h as n}from"./index.c27314ca.js";import{e as u,aC as p}from"./antdv.50a355b0.js";var f=u({setup(){const e=s(),o=n();return p(()=>{const{params:t,query:r}=e,{path:a}=t;o.replace({path:"/"+a,query:r})}),()=>""}});export{f as default};
---
+++ assets/index.225079f5.js
@@ -0,0 +1,1 @@
+import{j as s,h as n}from"./index.73cc9440.js";import{e as u,aC as p}from"./antdv.cb94fff7.js";var f=u({setup(){const e=s(),o=n();return p(()=>{const{params:t,query:r}=e,{path:a}=t;o.replace({path:"/"+a,query:r})}),()=>""}});export{f as default};
