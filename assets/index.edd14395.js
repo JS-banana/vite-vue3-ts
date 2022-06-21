946181f546c0b747c253eed1a29c9e3f70b5a450
JS-banana <JS-banana@users.noreply.github.com>
2022-06-21 14:53:32 +0000

    deploy: 2d093997a070feeb59bad059a59cc8eee8530907

---
+++ assets/index.edd14395.js
@@ -0,0 +1,1 @@
+import{j as s,h as n}from"./index.eb4f97cf.js";import{e as u,aC as p}from"./antdv.65c2eb16.js";var f=u({setup(){const e=s(),o=n();return p(()=>{const{params:t,query:r}=e,{path:a}=t;o.replace({path:"/"+a,query:r})}),()=>""}});export{f as default};
