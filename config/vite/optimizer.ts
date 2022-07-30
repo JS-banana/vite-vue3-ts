820c02e0f1eec256bf4fc9884cb25f2631fa803e
sunss <sss213018@163.com>
2022-07-30 22:27:16 +0800

    feat: vite2.x => vite3.x 工具链生态相关升级更新

--- config/vite/optimizer.ts
+++ config/vite/optimizer.ts
@@ -14,7 +14,8 @@ const vendorLibs: { match: string[]; output: string }[] = [
   },
 ];
 
-// @ts-ignore
+// pnpm安装的依赖，获取到的路径名称是拼接而成且比较长的
+// vite-vue3-ts/node_modules/.pnpm/registry.npmmirror.com+ant-design-vue@3.2.7_vue@3.2.23/node_modules/ant-design-vue/es/card/style/index.js
 export const configManualChunk = (id: string) => {
   if (/[\\/]node_modules[\\/]/.test(id)) {
     const matchItem = vendorLibs.find((item) => {
